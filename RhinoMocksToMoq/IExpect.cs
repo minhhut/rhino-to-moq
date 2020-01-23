4eeb74b79c556fe58a0b29ae745c92d55e35b57c
Minh Nguyen <minhhut@gmail.com>
2020-01-23 21:47:53 +0100

    First version

---
+++ RhinoMocksToMoq/IExpect.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Moq;
+
+namespace RhinoMocksToMoq
+{
+    public interface IExpect<T> where T : class
+    {
+        IRepeat<T> Repeat { get; set; }
+
+        void Throw(Exception exception);
+    }
+
+    public interface IExpect<T, TR> where T : class
+    {
+        void Return(TR result);
+
+        IRepeat<T, TR> Repeat { get; set; }
+    }
+    
+    public class Expect<T, TR> : IExpect<T, TR> where T : class
+    {
+        private readonly Mock<T> _mock;
+        private readonly Expression<Func<T, TR>> _expression;
+
+        private static ConcurrentDictionary<Tuple<object, string>, List<TR>> RESULT_STORE = new ConcurrentDictionary<Tuple<object, string>, List<TR>>();
+
+        public Expect(Mock<T> mock, Expression<Func<T, TR>> expression)
+        {
+            _mock = mock;
+            _expression = expression;
+
+            Repeat = new Repeat<T, TR>(this);
+        }
+
+        public void Return(TR result)
+        {
+            var repeat = (Repeat<T, TR>) Repeat;
+
+            switch (repeat.RepeatType)
+            {
+                case RepeatType.Any:
+                    _mock.Setup(_expression).Returns(result);
+                    break;
+                case RepeatType.Once:
+                    RESULT_STORE.AddOrUpdate(new Tuple<object, string>(_mock.Object, _expression.ToString()), expression =>
+                    {
+                        _mock.Setup(_expression).Returns(result);
+                        return new List<TR>() {result};
+                    }, (expression, resultList) =>
+                    {
+                        resultList.Add(result);
+                        _mock.Setup(_expression).ReturnsInOrder(resultList);
+                        return resultList;
+                    });
+                    break;
+                case RepeatType.AtLeastOnce:
+                    _mock.Setup(_expression).Returns(result).Verifiable("Should call at lease once");
+                    break;
+                case RepeatType.Never:
+                    _mock.Setup(_expression).Throws(new Exception("Should never be called"));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"{repeat.RepeatType} is not implemented");
+            }
+        }
+
+        public IRepeat<T, TR> Repeat { get; set; }
+    }
+
+    public class Expect<T> : IExpect<T> where T : class
+    {
+        private readonly Mock<T> _mock;
+
+        public IRepeat<T> Repeat { get; set; }
+
+        public Expression<Action<T>> Expression { get; }
+
+        public Expect(Mock<T> mock, Expression<Action<T>> expression)
+        {
+            this._mock = mock;            
+            Expression = expression;
+            Repeat = new Repeat<T>(mock, expression);
+        }
+
+        public void Throw(Exception exception)
+        {
+            _mock.Setup(Expression).Throws(exception);
+        }
+    }
+}
---
+++ RhinoMocksToMoq/MockRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Moq;
+
+namespace RhinoMocksToMoq
+{
+    public static class MockRepository
+    {
+        private static readonly ConcurrentDictionary<object, Mock> MOCK_STORE = new ConcurrentDictionary<object, Mock>();
+
+        public static Mock<T> Get<T>(T obj) where T : class
+        {
+            if (MOCK_STORE.TryGetValue(obj, out var mock))
+            {
+                return (Mock<T>) mock;
+            }
+            throw new ArgumentOutOfRangeException(nameof(obj), "Could not find object from mock collection");
+        }
+
+        public static T GenerateStub<T>() where T : class
+        {
+            return GenerateMock<T>();
+        }
+
+        public static T GenerateStrictMock<T>() where T : class
+        {
+            return GenerateMock<T>(MockBehavior.Strict);
+        }
+
+        public static T GenerateMock<T>(params object[] args) where T : class
+        {
+            var mock = new Mock<T>(args);
+            if (MOCK_STORE.TryAdd(mock.Object, mock))
+            {
+                return mock.Object;
+            }
+            throw new Exception("Failed to GenerateMock");
+        }
+
+        public static T GenerateMock<T>(MockBehavior behavior = MockBehavior.Default) where T : class
+        {
+            var mock = new Mock<T>(behavior);
+            if (MOCK_STORE.TryAdd(mock.Object, mock))
+            {
+                return mock.Object;
+            }
+            throw new Exception("Failed to GenerateMock");
+        }
+    }
+}
